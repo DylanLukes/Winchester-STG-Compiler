9179581d093951289dd6501924f4dea378c71dce
Dylan Lukes <lukes.dylan@gmail.com>
2012-01-11 11:35:20 -0500

    Initial Commit

---
+++ wsc/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  wsc
+//
+//  Created by Dylan Lukes on 1/11/12.
+//  Copyright (c) 2012 Dylan Lukes. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
