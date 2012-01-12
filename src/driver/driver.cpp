560e3bf1d4b13ac6cfda179f67603f7323839f3d
Dylan Lukes <lukes.dylan@gmail.com>
2012-01-11 20:16:36 -0500

    removed lexer. redo time

--- src/driver/driver.cpp
+++ src/driver/driver.cpp
@@ -6,12 +6,10 @@
 //  Copyright (c) 2012 Dylan Lukes. All rights reserved.
 //
 
-#include <iostream>
-
 int main (int argc, const char * argv[])
 {
     // insert code here...
-    std::cout << "Hello, World!\n";
+	
     return 0;
 }
 
