a9da3fde475386bb9f5d7e585c0c027d3662663b
Dylan Lukes <lukes.dylan@gmail.com>
2012-01-13 06:56:38 -0500

    more ast

--- src/driver/driver.cpp
+++ src/driver/driver.cpp
@@ -1,15 +1,9 @@
-//
-//  main.cpp
-//  wsc
-//
-//  Created by Dylan Lukes on 1/11/12.
-//  Copyright (c) 2012 Dylan Lukes. All rights reserved.
-//
+#include <syntax/AST.hpp>
 
 int main (int argc, const char * argv[])
 {
     // insert code here...
-	
+    
     return 0;
 }
 
